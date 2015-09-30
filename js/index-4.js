4042a4533530b5c0b1f5ca2568256ae200079ea7
danielsd10 <danielsd10@gmail.com>
2015-09-30 18:32:20 -0500

    gitignore, ejercicio 4

---
+++ js/index-4.js
@@ -0,0 +1,15 @@
+/*
+
+var fecha_hoy = new Date();
+
+var mañana = fecha.setDate(hoy.getDate() + 1);
+
+Math.floor() para redondear hacia arriba
+
+*/
+
+$(document).ready(function(){
+
+	$("#calcular").click();
+
+});
